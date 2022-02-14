5d0c9d720bb857bec2c780dd0e99c4c73edc5b8c
talkcryptome <talkcryptowithme@gmail.com>
2022-02-14 00:12:29 -0700

    Cleaning up Menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,12 +12,12 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.talkcryptotome.com/#/swap?outputCurrency=0x9066e87Bac891409D690cfEfA41379b34af06391',
+        href: 'https://exchange.talkcryptotome.com/#/swap',
       },
       {
         label: 'Liquidity',
         href:
-          'https://exchange.talkcryptotome.com/#/add/0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56/0x9066e87Bac891409D690cfEfA41379b34af06391',
+          'https://exchange.talkcryptotome.com/#/add',
       },
     ],
   },
